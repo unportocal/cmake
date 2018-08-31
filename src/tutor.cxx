794a050c7c9582656fc1dcbaba05941ae201c4b8
VBordea <VictorValentin.Bordea@harman.com>
2018-08-31 12:25:23 +0300

    added optional Library option. Actual lib implementation not added.

--- src/tutor.cxx
+++ src/tutor.cxx
@@ -2,15 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "inc/TutorialConfig.hpp"
+#ifdef USE_MYMATH
+#include "mathFunctions.hpp"
+#endif
+
 int main (int argc, char *argv[])
 {
+	fprintf(stdout,"%s Version %d.%d\n --=============--\n",
+            argv[0],
+            Tutorial_VERSION_MAJOR,
+            Tutorial_VERSION_MINOR);
   if (argc < 2)
     {
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
     }
   double inputValue = atof(argv[1]);
+#ifdef USE_MYMATH
+  double outputValue = mysqrt(inputValue);
+#else
   double outputValue = sqrt(inputValue);
+#endif
   fprintf(stdout,"The square root of %g is %g\n",
           inputValue, outputValue);
   return 0;
