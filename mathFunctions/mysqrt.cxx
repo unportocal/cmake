086d98679cf13ba30280fbc77b1a82830ba4250b
VBordea <VictorValentin.Bordea@harman.com>
2018-08-31 17:47:34 +0300

    added scripts and a small change

--- mathFunctions/mysqrt.cxx
+++ mathFunctions/mysqrt.cxx
@@ -1,4 +1,33 @@
 // mysqrt.hpp
+// Just a small change 
+/*
+#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+
+::vbo::victorvalentin.bordea@harman.com
+::adi::Activity Diagram
+::smdi::State Machine Diagram
+::tru::[true]
+::fls::[false]
+::wlex::writeLineEx(1,1, "Error");
+::ccomm::/** ************ `n`n ************ 
+::lcomm:://* 
+::ccline:://* ------------  ------------
+::cout::std`:`:cout <<
+::endl:: std`:`:endl;
+::svec::std`:`:vector<>
+::sthrd::std`:`:thread<>
+::@remove::/* ** @Victor - remove line ** */
+/*
+::start-sh2::start f:\Inst\teraterm\ttermpro.exe /F=f:\Inst\teraterm\Hermes_SH2.INI
+::start-nad::start f:\Inst\teraterm\ttermpro.exe /F=f:\Inst\teraterm\Hermes_NAD.INI
+::start-wuc::start f:\Inst\teraterm\ttermpro.exe /F=f:\Inst\teraterm\Hermes_WUC.INI
+::comit::git commit -m ""
+::gstat::git status`n
+*/
+
 #include "mathFunctions.hpp"
 
 #define EPSILON 1e-10
