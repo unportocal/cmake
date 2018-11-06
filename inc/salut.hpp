e84337ddde3f6396c3f403e78ca1f3c27f937e49
VBordea <victorvalentin.bordea@harman.com>
2018-11-06 13:44:48 +0000

    Preparing step2: hello world example which uses a different folder for source and include files

---
+++ inc/salut.hpp
@@ -0,0 +1,12 @@
+#ifndef __SALUT_HPP__
+#define __SALUT_HPP__
+
+class HelloClass {
+private:
+  /* data */
+
+public:
+  HelloClass (arguments);
+  void HelloPrint(/* void */);
+  virtual ~HelloClass ();
+};
---
+++ src/salut.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "salut.hpp"
+
+void HelloClass::HelloPrint()
+{
+  std::std::cout << "HelloClass says me. With headers and includes & sht" << '\n';
+}
