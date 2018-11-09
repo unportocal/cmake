edb2e85c50a6778bec7c401251b9e167e70990fc
VBordea <victorvalentin.bordea@harman.com>
2018-11-09 11:24:26 +0000

    CMake header file generation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,11 @@
 #include <iostream>
-#include "shared/salut.hpp"
+#include "myVer.h"
+#include "myPath.h"
 
 int main(int argc, char *argv[])
 {
-   std::cout << "Hello Install from main!" << std::endl;
-   HelloClass classInstace;
-   classInstace.HelloPrintShared();
+    std::cout << "Hello Version " << ver << "!" << std::endl;
+    std::cout << "Path is " << path << std::endl;
 
-   return 0;
+    return 0;
 }
-
-/*
-Variable	                                     Info
-
-CMAKE_SOURCE_DIR                      The root source directory
-
-CMAKE_CURRENT_SOURCE_DIR              The current source directory if using sub-projects and directories.
-
-PROJECT_SOURCE_DIR                    The source directory of the current cmake project.
-
-CMAKE_BINARY_DIR                      The root binary / build directory. This is the directory where you ran the cmake command.
-
-CMAKE_CURRENT_BINARY_DIR              The build directory you are currently in.
-
-PROJECT_BINARY_DIR                    The build directory for the current project.
-*/
